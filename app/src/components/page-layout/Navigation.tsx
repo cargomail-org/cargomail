d21ebc3495f265929f5d28f4fb29a9afe60f1b30
1token <zboran@federizer.com>
2022-08-22 12:47:44 +0200

    userMenu

--- app/src/components/page-layout/Navigation.tsx
+++ app/src/components/page-layout/Navigation.tsx
@@ -18,9 +18,11 @@ import { useNavigate } from 'react-router-dom'
 import { useConfig } from '../../packages/core/config'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
-import { Button, Hidden, Menu, MenuItem, Typography } from '@mui/material'
+import { Avatar, Button, Hidden, Menu, MenuItem, Tooltip, Typography } from '@mui/material'
 
 const drawerWidth = 240
+
+const settings = ['Account', 'Logout']
 
 interface AppBarProps extends MuiAppBarProps {
   open?: boolean
@@ -59,6 +61,16 @@ const DrawerMobile = styled(MuiDrawer)(({ theme }) => ({
 }))
 
 export function Navigation() {
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null)
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget)
+  }
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null)
+  }
+
   const [open, setOpen] = useState(window.innerWidth < 769 ? false : true)
   const toggleDrawer = () => {
     setOpen(!open)
@@ -110,7 +122,35 @@ export function Navigation() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             {productName}
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}>
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          {/* <Button color="inherit">Login</Button> */}
           {/* <AvatarMenu /> */}
           {/* {!isLoggedIn && (
             <>
