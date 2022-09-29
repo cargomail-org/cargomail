f8298261c505f9c31d46f4404f9e078e6252aeb2
1token <zboran@federizer.com>
2022-09-29 18:47:28 +0200

    lexical integration cont. (not finished yet)

--- app/src/components/editor/Editor.tsx
+++ app/src/components/editor/Editor.tsx
@@ -4,17 +4,22 @@ import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin'
 import { ContentEditable } from '@lexical/react/LexicalContentEditable'
 import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin'
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
+import { CheckListPlugin } from '@lexical/react/LexicalCheckListPlugin'
 import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin'
 import { ListPlugin } from '@lexical/react/LexicalListPlugin'
 import { HashtagPlugin } from '@lexical/react/LexicalHashtagPlugin'
 import EditorConfig from './EditorConfig'
 import EditorSetValue from './EditorSetValue'
-import EditorToolbar from './EditorToolbar'
+import ToolbarPlugin from './plugins/ToolbarPlugin'
 import EditorOnChange from './EditorOnChange'
 import MentionsPlugin from './plugins/MentionsPlugin'
 import CodeHighlightPlugin from './plugins/CodeHighlightPlugin'
 import AutoLinkPlugin from './plugins/AutoLinkPlugin'
 import FloatingLinkEditorPlugin from './plugins/FloatingLinkEditorPlugin'
+import HorizontalRulePlugin from './plugins/HorizontalRulePlugin'
+import ImagesPlugin from './plugins/ImagesPlugin'
+import EmojisPlugin from './plugins/EmojisPlugin'
+import KeywordsPlugin from './plugins/KeywordsPlugin'
 import Placeholder from './ui/Placeholder'
 import { useRef, useState } from 'react'
 
@@ -25,8 +30,8 @@ export interface EditorProps {
 }
 
 const Editor = ({ initialValue, onChange, editMode }: EditorProps) => {
-  const text = 'Enter some rich text...'
-  const placeholder = <Placeholder>{text}</Placeholder>
+  const text = 'Content...'
+  const placeholder = <Placeholder className="editor-placeholder">{text}</Placeholder>
   const scrollRef = useRef(null)
   const [floatingAnchorElem, setFloatingAnchorElem] = useState<HTMLDivElement | null>(null)
 
@@ -39,39 +44,38 @@ const Editor = ({ initialValue, onChange, editMode }: EditorProps) => {
   return (
     <LexicalComposer initialConfig={EditorConfig}>
       <div className="editor-container" ref={scrollRef}>
-        <div className="editor-inner">
-          <MentionsPlugin />
-          <HashtagPlugin />
-          {/* <RichTextPlugin
-            contentEditable={<ContentEditable className="editor-input" />}
-            placeholder={<Placeholder />}
-          /> */}
-          <AutoScrollPlugin scrollRef={scrollRef} />
-          <RichTextPlugin
-            contentEditable={
-              <div className="editor-scroller">
-                <div className="editor" ref={onRef}>
-                  <ContentEditable />
-                </div>
+        <MentionsPlugin />
+        <HashtagPlugin />
+        <KeywordsPlugin />
+        <EmojisPlugin />
+        <AutoScrollPlugin scrollRef={scrollRef} />
+        <RichTextPlugin
+          contentEditable={
+            <div className="editor-scroller">
+              <div className="editor" ref={onRef}>
+                <ContentEditable />
               </div>
-            }
-            placeholder={placeholder}
-          />
-          <EditorSetValue value={initialValue || ''} />
-          <OnChangePlugin onChange={(editorState, editor) => EditorOnChange(editorState, editor, onChange)} />
-          <HistoryPlugin />
-          <ListPlugin />
-          <LinkPlugin />
-          <AutoLinkPlugin />
-          <CodeHighlightPlugin />
-          {floatingAnchorElem && (
-            <>
-              <FloatingLinkEditorPlugin anchorElem={floatingAnchorElem} />
-            </>
-          )}
-        </div>
+            </div>
+          }
+          placeholder={placeholder}
+        />
+        <EditorSetValue value={initialValue || ''} />
+        <OnChangePlugin onChange={(editorState, editor) => EditorOnChange(editorState, editor, onChange)} />
+        <HistoryPlugin />
+        <CheckListPlugin />
+        <ListPlugin />
+        <LinkPlugin />
+        <AutoLinkPlugin />
+        <CodeHighlightPlugin />
+        <HorizontalRulePlugin />
+        <ImagesPlugin />
+        {floatingAnchorElem && (
+          <>
+            <FloatingLinkEditorPlugin anchorElem={floatingAnchorElem} />
+          </>
+        )}
+        <ToolbarPlugin />
       </div>
-      <EditorToolbar />
     </LexicalComposer>
   )
 }
