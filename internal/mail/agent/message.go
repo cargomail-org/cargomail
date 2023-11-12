ac1c0622a948f6487b8f89da06d2bc67fc5486ad
1token <zboran@federizer.com>
2023-11-12 19:38:07 +0100

    forward placeholder message (architecture)

--- internal/mail/agent/message.go
+++ internal/mail/agent/message.go
@@ -3,13 +3,13 @@ package agent
 import "cargomail/internal/repository"
 
 type UseMessageTransferAgent interface {
-	Send(user *repository.User, message *repository.Message) (*repository.Message, error)
+	Forward(user *repository.User, message *repository.Message) (*repository.Message, error)
 }
 
 type MessageTransferAgent struct {
 	repository repository.Repository
 }
 
-func (a *MessageTransferAgent) Send(user *repository.User, message *repository.Message) (*repository.Message, error) {
+func (a *MessageTransferAgent) Forward(user *repository.User, message *repository.Message) (*repository.Message, error) {
 	return nil, nil
 }
